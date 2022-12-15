1789ce96fb85bdf223f207e27351496df916b2e7
zix999 <maxkuks@outlook.com>
2022-12-15 09:39:05 +0300

    лаб 6

---
+++ 1.py
@@ -0,0 +1,6 @@
+def f1(n):
+    return f2(n)
+    
+def f2(n, k=0):
+    k+= n
+    
---
+++ 2.py
@@ -0,0 +1,13 @@
+def factorial(n):
+    k = 1
+    for i in range(n+1):
+        k*=i
+    return i
+    
+def operate(n, func):
+    return func(n)
+
+
+
+    
+    
